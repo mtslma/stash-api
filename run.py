99481e4ea8d8bbd605cdb327c66f5b1ce41c8cbb
Mateus Lima <Mateus Lima>
2025-08-21 12:36:39 -0300

    feat: Adapt run script for Render environment

--- run.py
+++ run.py
@@ -1,17 +1,20 @@
-# run.py
 import uvicorn
+import os # Importamos o 'os'
 from app.utils.network import get_local_ip
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
     local_ip = get_local_ip()
-    port = 8000
-    
+
     print("\n" + "="*50)
     print("🚀 INICIANDO SERVIDOR DA API 🚀")
-    print(f"\n✅ API estará acessível em: http://{local_ip}:{port}")
-    print("\n➡️  Endereço para requisições de download:")
-    print(f"   http://{local_ip}:{port}/api/extract")
+    if os.environ.get("RENDER"):
+        print("\n✅ Ambiente de Produção (Render) detetado.")
+        print(f"   Servidor a escutar na porta: {port}")
+    else:
+        print(f"\n✅ API acessível em: http://{local_ip}:{port}")
+        print(f"\n➡️  Copie este endereço: http://{local_ip}:{port}/api/extract")
     print("\n" + "="*50 + "\n")
     
-    # Diz ao uvicorn para rodar o objeto 'app' que está dentro do ficheiro 'main.py'
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
+    # Diz ao uvicorn para rodar na porta definida (10000 na Render, 8000 localmente)
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False) # 'reload=False' para produção
